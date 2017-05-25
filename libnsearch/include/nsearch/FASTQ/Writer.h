175cceaed751a0509ee61327f48cc4c5ae359557
Steven Schmid <stevschmid@gmail.com>
2017-05-25 10:20:02 +0200

    FASTQ Writer

---
+++ libnsearch/include/nsearch/FASTQ/Writer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../Sequence.h"
+
+#include <fstream>
+
+namespace FASTQ {
+  class Writer {
+  private:
+    std::ofstream mFile;
+    std::ostream &mOutput;
+
+  public:
+    Writer( std::ostream &output )
+      : mOutput( output )
+    {
+
+    }
+
+    Writer( const std::string &pathToFile )
+      : mFile( pathToFile ), mOutput( mFile )
+    {
+
+    }
+
+    void operator<<( const Sequence &seq ) {
+      mOutput << '@' << seq.identifier << std::endl;
+      mOutput << seq.sequence << std::endl;
+      mOutput << '+' << std::endl;
+      mOutput << seq.quality << std::endl;
+    }
+  };
+}
--- libnsearch/test/FASTQTest.cpp
+++ libnsearch/test/FASTQTest.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 
 #include <nsearch/FASTQ/Reader.h>
+#include <nsearch/FASTQ/Writer.h>
 
 #include <sstream>
 
@@ -31,5 +32,26 @@ TEST_CASE( "FASTQ" )  {
     REQUIRE( sequence.quality == "JAJI=" );
 
     REQUIRE( reader.EndOfFile() == true );
+  }
+
+  SECTION( "Writer" ) {
+    Sequence seq1( "Seq1", "TAGGC", "JJ:BB" );
+    Sequence seq2( "Seq2", "CTAGG", "AA..D" );
+
+    std::ostringstream oss;
+    FASTQ::Writer writer( oss );
+
+    writer << seq1;
+    writer << seq2;
+
+    const char *expectedOutput = "@Seq1\n"
+                                 "TAGGC\n"
+                                 "+\n"
+                                 "JJ:BB\n"
+                                 "@Seq2\n"
+                                 "CTAGG\n"
+                                 "+\n"
+                                 "AA..D\n";
+    REQUIRE( oss.str() == expectedOutput );
   }
 }
