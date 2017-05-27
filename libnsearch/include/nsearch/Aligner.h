5a299701f87f0bae73a452d2b5e97b290c165551
Steven Schmid <stevschmid@gmail.com>
2017-05-27 18:13:37 +0200

    Aligner

---
+++ libnsearch/include/nsearch/Aligner.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Sequence.h"
+#include "Utils.h"
+
+typedef enum {
+  CIGAR_MATCH = 'M',
+  CIGAR_DELETION = 'D',
+  CIGAR_INSERTION = 'I',
+  CIGAR_SOFT_CLIP = 'S',
+  CIGAR_HARD_CLIP = 'H',
+} CigarOperation;
+
+typedef struct std::pair< CigarOperation, int > CigarPair;
+typedef std::deque< CigarPair > Cigar;
+
+typedef struct {
+  int score;
+  Cigar cigar;
+} GlobalAlignment;
+
+typedef struct {
+  int score;
+  size_t targetStart, targetEnd;
+  size_t queryStart, queryEnd;
+} LocalAlignment;
+
+typedef std::shared_ptr< void > QueryProfile;
+
+class Aligner {
+public:
+  Aligner( int matchScore = 1, int mismatchScore = -2,
+      int gapOpenPenalty = 10, int gapExtendPenalty = 1 );
+
+  // Local alignment is blazing fast but we don't get detailed alignment (no cigar)
+  // Cache QueryProfile for saem query to speed up local alignments
+  LocalAlignment LocalAlign( const Sequence &query, const Sequence& target, QueryProfile *queryProfile = NULL ) const;
+
+  // Global alignment is slower but we get detailed alignment
+  GlobalAlignment GlobalAlign( const Sequence &query, const Sequence& target ) const;
+
+private:
+  int mGapOpenPenalty, mGapExtendPenalty;
+  int8_t mScoringMatrix[ NUC_MATRIX_SIZE * NUC_MATRIX_SIZE ];
+};
+
+extern void PrettyPrintGlobalAlignment( std::ostream &os, const GlobalAlignment &aln, const Sequence &query, const Sequence &target );
+extern std::ostream& operator<<( std::ostream &os, const GlobalAlignment &aln );
+
---
+++ libnsearch/src/Aligner.cpp
@@ -0,0 +1,165 @@
+#include "nsearch/Aligner.h"
+
+#include <iostream>
+
+#include <ksw.h>
+
+std::string BinarifySequence( const Sequence &seq ) {
+  std::string binSeq = seq.sequence;
+  for( auto &ch : binSeq )
+    ch -= NUC_MIN_ASCII;
+  return binSeq;
+}
+
+
+Aligner::Aligner( int matchScore, int mismatchScore, int gapOpenPenalty, int gapExtendPenalty )
+  : mGapOpenPenalty( gapOpenPenalty ), mGapExtendPenalty( gapExtendPenalty )
+{
+  for( int y = 0; y < NUC_MATRIX_SIZE; y++ ) {
+    for( int x = 0; x < NUC_MATRIX_SIZE; x++ ) {
+      mScoringMatrix[ y * NUC_MATRIX_SIZE + x ] =
+        NUC_MATRIX[ y ][ x ] > 0 ? matchScore : mismatchScore;
+    }
+  }
+}
+
+LocalAlignment Aligner::LocalAlign( const Sequence &query, const Sequence& target, QueryProfile *queryProfile ) const {
+  std::string binQuery = BinarifySequence( query );
+  std::string binTarget = BinarifySequence( target );
+
+  // If one query is aligned against multiple
+  // target sequences, *qry should be set to NULL during the first call and
+  // freed after the last call.
+  kswq_t *qry = NULL;
+  if( queryProfile ) {
+    qry = ( kswq_t* )(*queryProfile).get();
+  }
+
+  kswr_t result = ksw_align(
+      binQuery.length(),
+      (uint8_t*)&binQuery[ 0 ],
+
+      binTarget.length(),
+      (uint8_t*)&binTarget[ 0 ],
+
+      NUC_MATRIX_SIZE,
+      mScoringMatrix,
+
+      mGapOpenPenalty,
+      mGapExtendPenalty,
+      KSW_XSTART,
+      &qry );
+
+  LocalAlignment aln;
+  aln.score = result.score;
+  aln.targetStart = result.tb;
+  aln.targetEnd = result.te;
+  aln.queryStart = result.qb;
+  aln.queryEnd = result.qe;
+
+  if( queryProfile ) {
+    if( (*queryProfile).get() != qry ) {
+      *queryProfile = QueryProfile( qry, [=]( void *ptr ) { free( ptr ); } );
+    }
+  } else {
+    free( qry );
+  }
+
+  return aln;
+}
+
+GlobalAlignment Aligner::GlobalAlign( const Sequence &query, const Sequence& target ) const {
+  std::string binQuery = BinarifySequence( query );
+  std::string binTarget = BinarifySequence( target );
+
+  int queryLen = binQuery.length();
+  int targetLen = binTarget.length();
+  int bandWidth = std::max< int >( 50, 1.5 * abs( queryLen - targetLen ) );
+
+  int numCigar;
+  uint32_t *cigar;
+
+  int global = ksw_global(
+      queryLen,
+      (uint8_t*)&binQuery[ 0 ],
+
+      targetLen,
+      (uint8_t*)&binTarget[ 0 ],
+
+      NUC_MATRIX_SIZE,
+      mScoringMatrix,
+
+      mGapOpenPenalty,
+      mGapExtendPenalty,
+
+      bandWidth,
+      &numCigar,
+      &cigar );
+
+  GlobalAlignment aln;
+  aln.score = global;
+
+  for( int i = 0; i < numCigar; i++ ) {
+    int num = cigar[ i ] >> 4;
+    int op = cigar[ i ] & 0b1111;
+    CigarPair cp;
+    switch( op ) {
+      case 0: cp.first = CIGAR_MATCH; break;
+      case 1: cp.first = CIGAR_INSERTION; break;
+      case 2: cp.first = CIGAR_DELETION; break;
+    }
+    cp.second = num;
+    aln.cigar.push_back( std::move( cp ) );
+  }
+
+  // delete cigar
+  free( cigar );
+
+  return aln;
+}
+
+std::ostream& operator<<( std::ostream &os, const GlobalAlignment &aln ) {
+  os << "Score " << aln.score << " Cigar ";
+  for( auto &p : aln.cigar ) {
+    os << (int)p.second << (char)p.first;
+  }
+  return os;
+}
+
+void PrettyPrintGlobalAlignment( std::ostream &os, const GlobalAlignment &aln, const Sequence &query, const Sequence &target ) {
+  int qcount = 0;
+  int tcount = 0;
+
+  std::string q;
+  std::string t;
+
+  for( auto &p : aln.cigar ) {
+    CigarOperation op = p.first;
+    int len = p.second;
+
+    for( int i = 0; i < len; i++ ) {
+      switch( op ) {
+        case 'I':
+          t += '.';
+          q += query[ qcount++ ];
+          break;
+
+        case 'D':
+          q += '.';
+          t += target[ tcount++ ];
+          break;
+
+        case 'M':
+          q += query[ qcount++ ];
+          t += target[ tcount++ ];
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  os << "REF " << t << std::endl;
+  os << "QRY " << q << std::endl;
+}
