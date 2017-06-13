6ec24b7cfbf8bd90792768ab59994f545530e933
Steven Schmid <stevschmid@gmail.com>
2017-06-13 10:08:13 +0200

    Kmer class

--- libnsearch/include/nsearch/Database.h
+++ libnsearch/include/nsearch/Database.h
@@ -6,38 +6,7 @@
 #include "Sequence.h"
 #include "Utils.h"
 #include "Aligner.h"
-
-struct SequenceHasher {
-  size_t operator()( const std::string& sequence ) const {
-    // Word has to fit
-    assert( sequence.size() * 2 <= sizeof( size_t ) * 8 );
-
-    size_t key = 0;
-
-    for( int k = 0; k < sequence.size(); k++ ) {
-      int val = 0;
-      switch( sequence[ k ] ) {
-        case 'A': val = 0b00; break;
-        case 'C': val = 0b01; break;
-        case 'T': val = 0b10; break;
-        case 'G': val = 0b11; break;
-        default: assert( false ); break;
-      }
-
-      key |= ( val << k * 2 );
-    }
-
-    return key;
-  }
-};
-
-typedef struct {
-  int score;
-  int mismatches;
-  double identity;
-  Cigar cigar;
-  std::string quality;
-} Hit;
+#include "Kmer.h"
 
 class Database {
   typedef std::pair< size_t, const Sequence* > SequenceInfo;
@@ -56,16 +25,19 @@ public:
     const Sequence *ref = &mSequences.back();
 
     // Map words
-    ForEveryWordInSequence( *ref, [&]( size_t pos, std::string& word ) {
-      this->mWords[ word ].push_back( SequenceInfo( pos, ref ) );
+    Kmers kmers( *ref, mWordSize );
+    kmers.ForEach( [&]( const Sequence &kmer, size_t pos ) {
+      this->mWords[ kmer ].push_back( SequenceInfo( pos, ref ) );
     });
   }
 
   SequenceList Query( const Sequence &query, int maxHits = 10 ) const {
     std::unordered_map< const Sequence*, Coverage > candidates;
 
-    ForEveryWordInSequence( query, [&]( size_t pos, std::string &word ) {
-      for( auto &seqInfo : mWords.at( word ) ) {
+    // Go through each kmer, find candidates
+    Kmers kmers( query, mWordSize );
+    kmers.ForEach( [&]( const Sequence &kmer, size_t pos ) {
+      for( auto &seqInfo : mWords.at( kmer ) ) {
         size_t candidatePos = seqInfo.first;
         const Sequence *candidateSeq = seqInfo.second;
 
@@ -78,10 +50,12 @@ public:
       }
     });
 
+    // Sort candidates by coverage
     std::set< std::pair< Coverage, const Sequence* > > sortedCandidates;
     for( auto &c : candidates )
       sortedCandidates.insert( std::pair< Coverage, const Sequence* >( c.second, c.first ) );
 
+    // Now score the candidates
     SequenceList list;
     for( auto it = sortedCandidates.rbegin(); it != sortedCandidates.rend(); ++it ) {
       /* std::cout << "Candidate " << (*it).first.CoveredFraction() << std::endl; */
@@ -100,41 +74,7 @@ public:
   }
 
 private:
-  void ForEveryWordInSequence( const Sequence &seq, const std::function< void( size_t, std::string& ) > &block ) const {
-    int wordLength = std::min< int >( seq.Length(), mWordSize );
-    for( int i = 0; i <= seq.Length() - wordLength; i++ ) {
-      std::string word = seq.sequence.substr( i, wordLength );
-      ForEveryWordVariation( block, word, i );
-    }
-  }
-
-  void ForEveryWordVariation( const std::function< void( size_t, std::string& ) > &block,
-      std::string &word, size_t global, size_t start = 0 ) const
-  {
-    static const char BASES[] = "ATCG";
-
-    // Find first ambiguous nucleotide
-    size_t pos = word.find_first_not_of( BASES, start );
-    if( pos != std::string::npos ) {
-      bool skip = false;
-
-      if( !skip ) {
-        // Iterate through all possible bases
-        char nuc = word[ pos ];
-        for( int i = 0; i < sizeof( BASES ) - 1; i++ ) {
-          if( DoNucleotidesMatch( nuc, BASES[ i ] ) )  {
-            word[ pos ] = BASES[ i ];
-            ForEveryWordVariation( block, word, global, pos + 1 );
-          }
-        }
-      }
-    } else {
-      block( global, word );
-    }
-  }
-
   int mWordSize;
-
   SequenceList mSequences;
-  std::unordered_map< std::string, std::deque< SequenceInfo >, SequenceHasher > mWords;
+  std::unordered_map< Sequence, std::deque< SequenceInfo > > mWords;
 };
