2b2e9c04e75581ef1fa1d3e5404ddc60bb4849ec
Steven Schmid <stevschmid@gmail.com>
2017-06-21 14:48:55 +0200

    💅

--- libnsearch/include/nsearch/Alignment/DP.h
+++ libnsearch/include/nsearch/Alignment/DP.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <cassert>
 
+#include "../Utils.h"
+
 #define MININT -INT_MIN/2 //prevent underflow
 
 class AlignmentParams {
@@ -31,96 +33,23 @@ public:
   }
 };
 
-class BandedDP
-{
+class DPAlign {
 public:
-  BandedDP( const std::string &A, const std::string &B, size_t bandWidth, AlignmentParams ap = AlignmentParams() )
-    : mWidth( A.length() + 1 ), mHeight( B.length() + 1 ),
-      mSequenceA( A ), mSequenceB( B ),
-      mBandWidth( bandWidth ),
-      mAP( ap )
+  DPAlign( const Sequence &A, const Sequence &B, AlignmentParams ap = AlignmentParams() )
+    : mSequenceA( A ), mSequenceB( B ), mAP( ap )
   {
     // Todo: Intelligent cache
+    mWidth = A.Length() + 1;
+    mHeight = B.Length() + 1;
     mCells = new Cell[ mWidth * mHeight ];
   }
 
-  ~BandedDP() {
+  virtual ~DPAlign() {
     delete[] mCells;
   }
 
-  void Do() {
-    size_t rowWidth;
-    size_t x, y;
-
-    cell( 0, 0 ).score = 0;
-
-    for( x = 1; x < mWidth; x++ ) {
-      if( x > mBandWidth )
-        break;
-      ComputeCell( x, 0 );
-    }
-
-    size_t xFirst;
-    size_t xLast;
-
-    size_t lastCursor = 0;
-
-    for( size_t y = 1; y < mHeight; y++ ) {
-      size_t cursor = mWidth * float( y + 1 ) / float( mHeight );
-
-      size_t xFirst = ( cursor > mBandWidth ) ? ( cursor - mBandWidth ) : 0;
-      size_t xLast = ( cursor + mBandWidth < mWidth - 1 ) ? ( cursor + mBandWidth ) : ( mWidth - 1 );
-
-      if( xFirst > lastCursor ) {
-        xFirst = lastCursor;
-      }
-
-      std::cout << "xfirst " << xFirst << " cursor " << cursor << " xlast " << xLast << std::endl;
-
-      for( size_t x = xFirst; x <= xLast; x++ ) {
-        ComputeCell( x, y );
-      } // for x
-
-      lastCursor = cursor;
-    } // for y
-  }
-
-  void ComputeCell( size_t x, size_t y ) {
-    Cell& cur = cell( x, y );
-
-    if( x == 0 && y == 0 ) {
-      cur.score = cur.vGap = cur.hGap = 0;
-      return;
-    }
-
-    if( y == 0 ) {
-      cur.score = cur.hGap = mAP.GapScore( x, true );
-      return;
-    }
-
-    if( x == 0 ) {
-      cur.score = cur.vGap = mAP.GapScore( y, true );
-      return;
-    }
-
-    const Cell& leftCell = cell( x - 1, y );
-    const Cell& upperCell = cell( x, y - 1 );
-    const Cell& leftUpperCell = cell( x - 1, y - 1 );
-
-    cur.hGap = std::max(
-        leftCell.score + mAP.GapScore( 1, x == mWidth - 1 ),
-        leftCell.hGap + mAP.GapExtensionScore( 1, x == mWidth - 1 ) );
-
-    cur.vGap = std::max(
-        upperCell.score + mAP.GapScore( 1, y == mHeight -1 ),
-        upperCell.vGap + mAP.GapExtensionScore( 1, y == mHeight - 1 ) );
-
-    int diagScore = leftUpperCell.score + ( mSequenceA[ x - 1 ] == mSequenceB[ y - 1 ] ? mAP.matchScore : mAP.mismatchScore );
-    cur.score = std::max( { diagScore, cur.hGap, cur.vGap } );
-  }
-
   void DebugPrint( bool withArrows = false ) {
-    for( int x = 0; x < mWidth; x++ ) {
+    for( size_t x = 0; x < mWidth; x++ ) {
       if( x == 0 ) {
         printf( "      " );
       } else {
@@ -129,8 +58,8 @@ public:
     }
     printf( "\n");
 
-    for( int y = 0; y < mHeight; y++ ) {
-      for( int x = 0; x < mWidth; x++ ) {
+    for( size_t y = 0; y < mHeight; y++ ) {
+      for( size_t x = 0; x < mWidth; x++ ) {
         if( x == 0 ) {
           char ch = ' ';
           if( y > 0 ) {
@@ -162,7 +91,46 @@ public:
     }
   }
 
-private:
+protected:
+  /* virtual const Cell& TracebackStartingCell() const = 0; */
+  virtual void ComputeMatrix() = 0;
+
+  void Traceback();
+  void ComputeCell( size_t x, size_t y ) {
+    Cell& cur = cell( x, y );
+
+    if( x == 0 && y == 0 ) {
+      cur.score = cur.vGap = cur.hGap = 0;
+      return;
+    }
+
+    if( y == 0 ) {
+      cur.score = cur.hGap = mAP.GapScore( x, true );
+      return;
+    }
+
+    if( x == 0 ) {
+      cur.score = cur.vGap = mAP.GapScore( y, true );
+      return;
+    }
+
+    const Cell& leftCell = cell( x - 1, y );
+    const Cell& upperCell = cell( x, y - 1 );
+    const Cell& leftUpperCell = cell( x - 1, y - 1 );
+
+    cur.hGap = std::max(
+        leftCell.score + mAP.GapScore( 1, x == mWidth - 1 ),
+        leftCell.hGap + mAP.GapExtensionScore( 1, x == mWidth - 1 ) );
+
+    cur.vGap = std::max(
+        upperCell.score + mAP.GapScore( 1, y == mHeight -1 ),
+        upperCell.vGap + mAP.GapExtensionScore( 1, y == mHeight - 1 ) );
+
+    int diagScore = leftUpperCell.score +
+      ( DoNucleotidesMatch( mSequenceA[ x - 1 ], mSequenceB[ y - 1 ] ) ? mAP.matchScore : mAP.mismatchScore );
+    cur.score = std::max( { diagScore, cur.hGap, cur.vGap } );
+  }
+
   struct Cell {
     int score = MININT;
     int vGap = MININT;
@@ -175,9 +143,53 @@ private:
     return mCells[ y * mWidth + x ];
   }
 
-  size_t mBandWidth;
   size_t mWidth, mHeight;
-  std::string mSequenceA, mSequenceB;
+  Sequence mSequenceA, mSequenceB;
   Cell *mCells;
   AlignmentParams mAP;
+};
+
+class BandedDPAlign : public DPAlign
+{
+public:
+  BandedDPAlign( const Sequence &A, const Sequence &B, AlignmentParams ap, size_t bandWidth )
+    : DPAlign( A, B, ap ), mBandWidth( bandWidth )
+  {
+  }
+
+  void ComputeMatrix() {
+    for( size_t x = 0; x < mWidth; x++ ) {
+      if( x > mBandWidth )
+        break;
+
+      ComputeCell( x, 0 );
+    }
+
+    size_t xFirst;
+    size_t xLast;
+
+    size_t lastCursor = 0;
+
+    for( size_t y = 1; y < mHeight; y++ ) {
+      size_t cursor = mWidth * float( y + 1 ) / float( mHeight );
+
+      size_t xFirst = ( cursor > mBandWidth ) ? ( cursor - mBandWidth ) : 0;
+      size_t xLast = ( cursor + mBandWidth < mWidth - 1 ) ? ( cursor + mBandWidth ) : ( mWidth - 1 );
+
+      if( xFirst > lastCursor ) {
+        xFirst = lastCursor;
+      }
+
+      std::cout << "xfirst " << xFirst << " cursor " << cursor << " xlast " << xLast << std::endl;
+
+      for( size_t x = xFirst; x <= xLast; x++ ) {
+        ComputeCell( x, y );
+      } // for x
+
+      lastCursor = cursor;
+    } // for y
+  }
+
+protected:
+  size_t mBandWidth;
 };
