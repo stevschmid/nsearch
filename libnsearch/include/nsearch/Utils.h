08913bfcdc805fbd616f3d7ec4b44911016d6443
Steven Schmid <stevschmid@gmail.com>
2017-06-21 11:24:09 +0200

    RangeMerger

--- libnsearch/include/nsearch/Utils.h
+++ libnsearch/include/nsearch/Utils.h
@@ -4,7 +4,6 @@
 #include <cassert>
 #include <ctype.h>
 #include <numeric>
-#include <map>
 
 static bool IsBlank( const std::string &str ) {
   return str.empty() || std::all_of( str.begin(), str.end(), isspace );
@@ -81,52 +80,3 @@ static inline char NucleotideComplement( char nuc ) {
 
   return nuc;
 }
-
-class Coverage {
-public:
-  size_t NumNonOverlaps() const {
-    return mRanges.size();
-  }
-
-  size_t CoveredSize() const {
-    size_t coveredSize = 0;
-    for( auto &range : mRanges ) {
-      coveredSize += ( range.second - range.first ) + 1;
-    }
-    return coveredSize;
-  }
-
-  void Add( size_t rangeMin, size_t rangeMax ) {
-    assert( rangeMin <= rangeMax );
-
-    std::map< size_t, size_t >::iterator insertIt, afterIt = mRanges.upper_bound( rangeMin );
-
-    if( afterIt == mRanges.begin() || std::prev( afterIt )->second < rangeMin ) {
-      insertIt = mRanges.insert( afterIt, std::pair< size_t, size_t >( rangeMin, rangeMax ) );
-    } else {
-      insertIt = std::prev( afterIt );
-      if( insertIt->second < rangeMax ) {
-        // Extend overlap
-        insertIt->second = rangeMax;
-      }
-    }
-
-    // Merge following ranges if covered fully by this one
-    while( afterIt != mRanges.end() && rangeMax >= afterIt->first ) {
-      insertIt->second = std::max( afterIt->second, insertIt->second );
-      afterIt = mRanges.erase( afterIt );
-    }
-  }
-
-  bool operator<( const Coverage &other ) const {
-    return CoveredSize() < other.CoveredSize();
-  }
-
-  const std::map< size_t, size_t >& Ranges() const {
-    return mRanges;
-  }
-
-private:
-  std::map< size_t, size_t > mRanges; // Non overlapping ranges
-};
-
