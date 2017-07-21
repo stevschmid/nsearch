5e9cfc7bf9271364ea566f6e9a7e9aa9324357ac
Steven Schmid <stevschmid@gmail.com>
2017-07-21 12:20:25 +0200

    Cleanup

--- libnsearch/include/nsearch/Alignment/HitTracker.h
+++ libnsearch/include/nsearch/Alignment/HitTracker.h
@@ -7,40 +7,27 @@
 
 class HitTracker
 {
-  typedef std::pair< size_t, size_t > Pos;
+  using Diagonal = size_t;
 
 public:
   void AddHit( size_t start1, size_t start2, size_t length ) {
-    size_t diagStart1 = 0;
-    size_t diagStart2 = start2 - start1;
-    Ranges &ranges = mDiagonals[ Pos( diagStart1, diagStart2 ) ];
+    Diagonal diag = start2 - start1;
+    Ranges &ranges = mDiagonals[ diag ];
     ranges.Add( start1, start1 + length );
   }
-
-  size_t Score() const {
-    size_t score = 0;
-
-    for( auto &it : mDiagonals ) {
-      for( auto &range : it.second.Get() ) {
-        score += (range.second - range.first);
-      }
-    }
-
-    return score;
-  };
 
   SeedList Seeds() const {
     SeedList seeds;
 
     for( auto &it : mDiagonals ) {
-      auto &key = it.first;
+      auto &diag = it.first;
       auto &ranges = it.second;
 
       for( auto &range : ranges.Get() ) {
         // (6,1) -> (8,3)
         size_t length = range.second - range.first;
         size_t start1 = range.first;
-        size_t start2 = key.second + start1;
+        size_t start2 = diag + start1;
         seeds.push_back( Seed( start1, start2, length ) );
       }
     }
@@ -48,10 +35,6 @@ public:
     return seeds;
   };
 
-  bool operator<( const HitTracker &other ) const {
-    return Score() < other.Score();
-  }
-
 private:
-  std::map< Pos, Ranges > mDiagonals;
+  std::unordered_map< Diagonal, Ranges > mDiagonals;
 };
